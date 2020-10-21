f3773225b035b16ba9206789d8816d3cf3db33b5
Andrés Sánchez <anfurtivo1@hotmail.com>
2020-10-21 10:26:26 +0200

    Agregar archivos de proyecto.

---
+++ FormularioExcepciones/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace FormularioExcepciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txmMovil = new System.Windows.Forms.MaskedTextBox();
+            this.txmMatricula = new System.Windows.Forms.MaskedTextBox();
+            this.txmNIF = new System.Windows.Forms.MaskedTextBox();
+            this.txmCuenta = new System.Windows.Forms.MaskedTextBox();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.epNombre = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epApellidos = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epMovil = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epEmail = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epMatricula = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epNIF = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epCuenta = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.epNombre)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epApellidos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epMovil)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epEmail)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epMatricula)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epNIF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCuenta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(105, 69);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(185, 22);
+            this.txtNombre.TabIndex = 0;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(105, 111);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(387, 22);
+            this.txtApellido.TabIndex = 1;
+            this.txtApellido.TextChanged += new System.EventHandler(this.txtApellido_TextChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(105, 195);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(387, 22);
+            this.txtEmail.TabIndex = 2;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            // 
+            // txmMovil
+            // 
+            this.txmMovil.Location = new System.Drawing.Point(105, 156);
+            this.txmMovil.Mask = "000-000-000";
+            this.txmMovil.Name = "txmMovil";
+            this.txmMovil.Size = new System.Drawing.Size(162, 22);
+            this.txmMovil.TabIndex = 3;
+            this.txmMovil.TextChanged += new System.EventHandler(this.txmMovil_TextChanged);
+            // 
+            // txmMatricula
+            // 
+            this.txmMatricula.Location = new System.Drawing.Point(167, 252);
+            this.txmMatricula.Mask = "0000-LLL";
+            this.txmMatricula.Name = "txmMatricula";
+            this.txmMatricula.Size = new System.Drawing.Size(100, 22);
+            this.txmMatricula.TabIndex = 4;
+            this.txmMatricula.TextChanged += new System.EventHandler(this.txmMatricula_TextChanged);
+            // 
+            // txmNIF
+            // 
+            this.txmNIF.Location = new System.Drawing.Point(392, 252);
+            this.txmNIF.Mask = "00000000-L";
+            this.txmNIF.Name = "txmNIF";
+            this.txmNIF.Size = new System.Drawing.Size(100, 22);
+            this.txmNIF.TabIndex = 5;
+            this.txmNIF.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.txmNIF_MaskInputRejected);
+            // 
+            // txmCuenta
+            // 
+            this.txmCuenta.Location = new System.Drawing.Point(167, 297);
+            this.txmCuenta.Mask = "000-0000.0000.000.0000000000";
+            this.txmCuenta.Name = "txmCuenta";
+            this.txmCuenta.Size = new System.Drawing.Size(325, 22);
+            this.txmCuenta.TabIndex = 6;
+            this.txmCuenta.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.txmCuenta_MaskInputRejected);
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(382, 361);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(110, 23);
+            this.btnValidar.TabIndex = 7;
+            this.btnValidar.Text = "VALIDAR";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Apellidos:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Movil:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "email:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Matricula Coche:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 300);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Nº de Cuenta:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(323, 257);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "NIF:";
+            // 
+            // epNombre
+            // 
+            this.epNombre.BlinkRate = 500;
+            this.epNombre.ContainerControl = this;
+            // 
+            // epApellidos
+            // 
+            this.epApellidos.ContainerControl = this;
+            // 
+            // epMovil
+            // 
+            this.epMovil.ContainerControl = this;
+            // 
+            // epEmail
+            // 
+            this.epEmail.ContainerControl = this;
+            // 
+            // epMatricula
+            // 
+            this.epMatricula.ContainerControl = this;
+            // 
+            // epNIF
+            // 
+            this.epNIF.ContainerControl = this;
+            // 
+            // epCuenta
+            // 
+            this.epCuenta.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.txmCuenta);
+            this.Controls.Add(this.txmNIF);
+            this.Controls.Add(this.txmMatricula);
+            this.Controls.Add(this.txmMovil);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Form1";
+            this.Text = "Datos Personales";
+            ((System.ComponentModel.ISupportInitialize)(this.epNombre)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epApellidos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epMovil)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epEmail)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epMatricula)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epNIF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCuenta)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.MaskedTextBox txmMovil;
+        private System.Windows.Forms.MaskedTextBox txmMatricula;
+        private System.Windows.Forms.MaskedTextBox txmNIF;
+        private System.Windows.Forms.MaskedTextBox txmCuenta;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ErrorProvider epNombre;
+        private System.Windows.Forms.ErrorProvider epApellidos;
+        private System.Windows.Forms.ErrorProvider epMovil;
+        private System.Windows.Forms.ErrorProvider epEmail;
+        private System.Windows.Forms.ErrorProvider epMatricula;
+        private System.Windows.Forms.ErrorProvider epNIF;
+        private System.Windows.Forms.ErrorProvider epCuenta;
+    }
+}
+
---
+++ FormularioExcepciones/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularioExcepciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            String texto;
+            texto=txtNombre.Text.ToString();
+            texto=texto.Trim();
+
+            if (texto.Equals(""))
+            {
+                epNombre.SetError(txtNombre,"Tiene que estar relleno");
+            }
+        }
+
+        private void txtApellido_TextChanged(object sender, EventArgs e)
+        {
+            String texto;
+            texto = txtApellido.Text.ToString();
+            texto = texto.Trim();
+
+            if (texto.Equals(""))
+            {
+                epNombre.SetError(txtApellido, "Tiene que estar relleno");
+            }
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+            String texto;
+            texto = txtEmail.Text.ToString();
+            texto = texto.Trim();
+
+            if (texto.Equals(""))
+            {
+                epNombre.SetError(txtEmail, "Tiene que estar relleno");
+            }
+        }
+
+        private void txmMovil_TextChanged(object sender, EventArgs e)
+        {
+            if (!txmMovil.MaskFull)
+            {
+                epMovil.SetError(txmMovil, "Tiene que estar relleno");
+            }
+        }
+
+        private void txmMatricula_TextChanged(object sender, EventArgs e)
+        {
+            if (!txmMatricula.MaskFull)
+            {
+                epMatricula.SetError(txmMatricula, "Tiene que estar relleno");
+            }
+        }
+
+        private void txmCuenta_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            if (!txmCuenta.MaskFull)
+            {
+                epMatricula.SetError(txmCuenta, "Tiene que estar relleno");
+            }
+        }
+
+        private void txmNIF_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            if (!txmNIF.MaskFull && !comprobarNIF(txmNIF.Text.ToString()))
+            {
+                epMatricula.SetError(txmNIF, "Tiene que estar relleno");
+            }
+        }
+
+        public static bool comprobarNIF(String NIF)
+        {
+            string[] letrasArray = new string[] { "T", "R", "W", "A", "G", "M", "Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E" };
+            String aux;
+            int aux2;
+            String aux3;
+            aux = NIF.Substring(0,NIF.Length-2);
+            aux3 = NIF.Substring(NIF.Length-1);
+            aux2 = Int32.Parse(aux);
+
+            aux2 = aux2 % 23;
+
+            if (letrasArray[aux2].Equals(aux3))
+            {
+                MessageBox.Show("Mensaje","El NIF es correcto");
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Mensaje", "El NIF no es correcto");
+                return false;
+            }
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            String texto1;
+            String texto2;
+            String texto3;
+            String texto4;
+            String texto5;
+            String texto6;
+            String texto7;
+
+            texto1 = txtNombre.Text.ToString();
+            texto1 = texto1.Trim();
+
+            texto2 = txmNIF.Text.ToString();
+            texto2 = texto2.Trim();
+
+            texto3 = txmMovil.Text.ToString();
+            texto3 = texto3.Trim();
+
+            texto4 = txmMatricula.Text.ToString();
+            texto4 = texto4.Trim();
+
+            texto5 = txtEmail.Text.ToString();
+            texto5 = texto5.Trim();
+
+            texto6 = txmCuenta.Text.ToString();
+            texto6 = texto6.Trim();
+
+            texto7 = txtApellido.Text.ToString();
+            texto7 = texto7.Trim();
+
+            comprobarNIF(txmNIF.Text.ToString());
+
+            if (!texto1.Equals(""))
+            {
+                epNombre.Clear();
+            }
+
+            if (texto2.Equals(""))
+            {
+                epNIF.Clear();
+            }
+
+            if (texto3.Equals(""))
+            {
+                epMovil.Clear();
+            }
+
+            if (texto4.Equals(""))
+            {
+                epMatricula.Clear();
+            }
+
+            if (texto5.Equals(""))
+            {
+                epEmail.Clear();
+            }
+
+            if (texto6.Equals(""))
+            {
+                epCuenta.Clear();
+            }
+
+            if (!texto7.Equals(""))
+            {
+                epApellidos.Clear();
+            }
+
+            
+        }
+    }
+}
